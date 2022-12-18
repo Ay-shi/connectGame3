36eb217fbd027af781a01fedd56a997e331c5aef
Ay-shi <98973417+Ay-shi@users.noreply.github.com>
2022-12-19 00:34:28 +0530

    win function and gamestate change function added

--- app/src/main/java/com/example/connectgame3/MainActivity.java
+++ app/src/main/java/com/example/connectgame3/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     int player=0;
     int[] gameState={2,2,2,2,2,2,2,2,2};
+    int [][] winningState={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void dropIn(View view){
-        if(didWin()) {
-            Toast.makeText(this,"you Won!!",Toast.LENGTH_LONG).show();
-            return;
-        }
-//        if(player==8) {
-//            Toast.makeText(this,"tie :(",Toast.LENGTH_LONG).show();
-//            player=0;
-//            return;
-//        }
+        gameState[Integer.parseInt(view.getTag().toString())]=player;
+
         ImageView component=(ImageView) view;
         component.setTranslationY(-1000);
         if (player == 0) {
@@ -37,9 +31,17 @@ public class MainActivity extends AppCompatActivity {
             component.setImageResource(R.drawable.zero);
         }
         component.animate().translationYBy(1000);
+
+        if(didWin()) {
+            Toast.makeText(this,"You Won!!",Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 
     public boolean didWin(){
+        for(int [] pos:winningState){
+            if(gameState[pos[0]]!=2 && gameState[pos[0]]==gameState[pos[1]] && gameState[pos[1]]==gameState[pos[2]]) return true;
+        }
         return false;
     }
 }
